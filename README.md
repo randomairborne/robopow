de91d1530dc8e71f299f7767860991070831ae7b
valkyrie_pilot <valk@randomairborne.dev>
2023-12-29 11:50:47 -0700

    Reformat with prettier

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 For shooting robots- pew pew!!
 
-Robopow is a CAPTCHA-like service that is designed to stop 
+Robopow is a CAPTCHA-like service that is designed to stop
 DDOS and large-scale automated attacks. It does **not** offer any
-protection against malicious users operating at a small scale, or 
+protection against malicious users operating at a small scale, or
 who have very large budgets. User beware.
 
 It uses a large number of sha512sum nonce calculations. It's kinda cool I guess.
@@ -20,17 +20,19 @@ Client JS:
 ```js
 // All optional
 const settings = {
-    zeros: 12, // How many zero bits are required
-    challenges: 8, // 0-prefixed shasums to require
-    timeout: 15 // timeout in seconds
+  zeros: 12, // How many zero bits are required
+  challenges: 8, // 0-prefixed shasums to require
+  timeout: 15, // timeout in seconds
 };
-const { token, nonces } = await Robopow.verifyCaptcha("https://robopow.valk.sh/api");
+const { token, nonces } = await Robopow.verifyCaptcha(
+  "https://robopow.valk.sh/api",
+);
 const request = await fetch(`http://yourapi.example.com/captcha/${token}`, {
-    method: 'POST',
-    body: JSON.stringify(nonces),
-    headers: {
-        "content-type": "application/json"
-    }
+  method: "POST",
+  body: JSON.stringify(nonces),
+  headers: {
+    "content-type": "application/json",
+  },
 });
 ```
 
@@ -41,27 +43,27 @@ const nonces = getRequestJson(); // As long as the order is preserved, you can t
 const token = getPathFragment(); // Ditto the above
 
 const request = await fetch(`https://robopow.valk.sh/api/v0/verify/${token}`, {
-    method: 'POST',
-    body: JSON.stringify(nonces), // Remember to preserve the order!!
-    headers: {
-        "content-type": "application/json"
-    }
+  method: "POST",
+  body: JSON.stringify(nonces), // Remember to preserve the order!!
+  headers: {
+    "content-type": "application/json",
+  },
 });
 const response = await request.json(); // Returns json object, documented below
 ```
 
-Validate that this response object is equal to the 
+Validate that this response object is equal to the
 configuration set in your client JS on your server.
 If valid is false, then the client did not properly
 solve the challenge, and you should deny the request.
 
 ```json
 {
-    "params": {
-        "zeros": 12,
-        "challenges": 8,
-        "timeout": 15
-    },
-    "valid": true
+  "params": {
+    "zeros": 12,
+    "challenges": 8,
+    "timeout": 15
+  },
+  "valid": true
 }
 ```
--- src/robopow.js
+++ src/robopow.js
@@ -1,58 +1,64 @@
 const Robopow = (function () {
-    function numToHex(number, radix, zeropad) {
-        return number.toString(radix).padStart(zeropad, "0")
+  function numToHex(number, radix, zeropad) {
+    return number.toString(radix).padStart(zeropad, "0");
+  }
+
+  async function findNonce(token, challenge, zerobits) {
+    const start = performance.now();
+    let nonce = 0;
+    while (true) {
+      const stringNonce = nonce;
+      const msgUint8 = new TextEncoder().encode(
+        stringNonce.toString() + token + challenge,
+      );
+      const hashBuffer = await crypto.subtle.digest("SHA-512", msgUint8);
+      const hashArray = Array.from(new Uint8Array(hashBuffer));
+      const bits = hashArray.map((n) => numToHex(n, 2, 8)).join("");
+      if (bits.startsWith("0".repeat(zerobits))) {
+        break;
+      }
+      nonce += 1;
     }
+    const end = performance.now();
+    const duration = end - start;
+    console.debug(
+      `Robopow round took ${duration}ms to find ${zerobits} zerobits`,
+    );
+    return nonce;
+  }
 
-    async function findNonce(token, challenge, zerobits) {
-        const start = performance.now();
-        let nonce = 0
-        while (true) {
-            const stringNonce = nonce;
-            const msgUint8 = new TextEncoder().encode(stringNonce.toString() + token + challenge);
-            const hashBuffer = await crypto.subtle.digest("SHA-512", msgUint8);
-            const hashArray = Array.from(new Uint8Array(hashBuffer));
-            const bits = hashArray
-                .map((n) => numToHex(n, 2, 8))
-                .join("");
-            if (bits.startsWith("0".repeat(zerobits))) {
-                break;
-            }
-            nonce += 1;
-        }
-        const end = performance.now();
-        const duration = end - start;
-        console.debug(`Robopow round took ${duration}ms to find ${zerobits} zerobits`);
-        return nonce;
+  async function verifyCaptcha(apiUrl, config) {
+    const configParams = new URLSearchParams(config).toString();
+    const challengeRequest = await fetch(
+      `${apiUrl}/v0/challenge?${configParams}`,
+    );
+    const resp = await challengeRequest.json();
+    const token = resp["token"];
+    const params = resp["params"];
+    const challenges = resp["challenges"];
+    const zeros = params["zeros"];
+
+    let challengeResponses = [];
+    const start = performance.now();
+    for (let i = 0; i < challenges.length; i++) {
+      const challenge = challenges[i];
+      const nonce = await findNonce(token, challenge, zeros);
+      challengeResponses.push(nonce);
     }
-
-    async function verifyCaptcha(apiUrl, config) {
-        const configParams = new URLSearchParams(config).toString();
-        const challengeRequest = await fetch(`${apiUrl}/v0/challenge?${configParams}`);
-        const resp = await challengeRequest.json();
-        const token = resp['token'];
-        const params = resp['params'];
-        const challenges = resp['challenges'];
-        const zeros = params['zeros'];
-
-        let challengeResponses = [];
-        const start = performance.now();
-        for (let i = 0; i < challenges.length; i++) {
-            const challenge = challenges[i];
-            const nonce = await findNonce(token, challenge, zeros);
-            challengeResponses.push(nonce);
-        }
-        const end = performance.now();
-        const duration = end - start;
-        console.log(`Robopow round took ${duration}ms to solve ${challenges.length} challenges`);
-        console.debug(JSON.stringify(challengeResponses))
-        return {
-            params,
-            token,
-            nonces: challengeResponses
-        }
-    }
-
+    const end = performance.now();
+    const duration = end - start;
+    console.log(
+      `Robopow round took ${duration}ms to solve ${challenges.length} challenges`,
+    );
+    console.debug(JSON.stringify(challengeResponses));
     return {
-        verifyCaptcha
-    }
-}());
+      params,
+      token,
+      nonces: challengeResponses,
+    };
+  }
+
+  return {
+    verifyCaptcha,
+  };
+})();
